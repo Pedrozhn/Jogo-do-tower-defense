956b7928d076e09993af08ea1f505e338a15e04b
Pedrozhn <pedrohenriquenagy@gmail.com>
2024-11-01 15:29:10 -0300

    mais scripts e torretas criadas

--- Tarefa Tower Defense/Assets/Scripts/Bullet.cs
+++ Tarefa Tower Defense/Assets/Scripts/Bullet.cs
@@ -15,17 +15,17 @@ public class Bullet : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private float bulletSpeed = 5f;
     [SerializeField] public float bulletdamage = 1f;
-    [SerializeField] private float lifetime = 5f; 
+    [SerializeField] private float lifetime = 5f;
 
-    private Transform target; 
+    private Transform target;
 
     private void Start()
     {
-       
+
         Destroy(gameObject, lifetime);
     }
 
-   
+
     public void SetTarget(Transform _target)
     {
         target = _target;
@@ -33,29 +33,29 @@ public class Bullet : MonoBehaviour
 
     private void FixedUpdate()
     {
-       
+
         if (!target) return;
 
-       
+
         Vector2 direction = (target.position - transform.position).normalized;
 
-       
+
         rb.velocity = direction * bulletSpeed;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-       
+
         Health enemyHealth = other.gameObject.GetComponent<Health>();
 
 
         if (enemyHealth != null)
         {
-            enemyHealth.TakeDamage(bulletdamage); 
-           
+            enemyHealth.TakeDamage(bulletdamage);
+
         }
 
-       
+
         Destroy(gameObject);
     }
 }
