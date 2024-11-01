60dea85e290e109ff1d97952d5b0a31ef9001a9b
Pedrozhn <pedrohenriquenagy@gmail.com>
2024-11-01 14:54:54 -0300

    criei os prefabs dos inimigos e das torres, e estou mexendo no visual. Criei o script health, e estou me preparando para concluir ele

--- Tarefa Tower Defense/Assets/LevelManager.cs
+++ Tarefa Tower Defense/Assets/LevelManager.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
+public class LevelManager : MonoBehaviour 
 {
-    // Start is called before the first frame update
-    void Start()
+    public static LevelManager instance; 
+
+    public Transform startPoint;
+
+    public Transform[] path;  
+    [SerializeField] public int currency;  
+
+    private void Awake()  
     {
-        
+        instance = this; 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start() 
     {
-        
+        currency = 500;
+    }
+
+    public void IncreaseCurrency(int amount) 
+    {
+        amount = 50; 
+        currency += amount; 
+    }
+
+    public bool SpendCurrency(int amount) 
+    {
+        if (amount <= currency) 
+        {
+            currency -= amount; 
+            return true; 
+        }
+        else
+        {
+            Debug.Log("You do not have enough to purchase this item"); 
+            return false; 
+        }
     }
 }
