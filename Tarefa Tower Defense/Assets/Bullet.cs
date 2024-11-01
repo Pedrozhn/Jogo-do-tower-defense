328349f94b1db592ad9de99c85700e9116bfa520
Pedrozhn <pedrohenriquenagy@gmail.com>
2024-11-01 14:01:01 -0300

    Fiz os códigos da torreta e da bala mas ainda está faltando outros scripts

---
+++ Tarefa Tower Defense/Assets/Bullet.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public static Bullet instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb; 
+
+    [Header("Attributes")]
+    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] public float bulletdamage = 1f; 
+    [SerializeField] private float lifetime = 5f; 
+
+    private Transform target; 
+
+    private void Start()
+    {
+        
+        Destroy(gameObject, lifetime);
+    }
+
+    
+    public void SetTarget(Transform _target)
+    {
+        target = _target; 
+    }
+
+    private void FixedUpdate()
+    {
+        
+        if (!target) return;
+
+       
+        Vector2 direction = (target.position - transform.position).normalized;
+
+       
+        rb.velocity = direction * bulletSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+       
+        Health enemyHealth = other.gameObject.GetComponent<Health>();
+
+        
+        if (enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(bulletdamage); 
+            
+        }
+
+       
+        Destroy(gameObject);
+    }
+}
---
+++ Tarefa Tower Defense/Assets/LevelManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;    
+
+    public Transform startPoint;    
+
+    public Transform[] path;   
+    [SerializeField] public int currency;    
+
+    private void Awake()   
+    {
+        instance = this; 
+    }
+
+    private void Start() 
+    {
+        currency = 500;
+    }
+
+    public void IncreaseCurrency(int amount) 
+    {
+        amount = 50; 
+        currency += amount; 
+    }
+
+    public bool SpendCurrency(int amount) 
+    {
+        if (amount <= currency) 
+        {
+            currency -= amount;
+            return true;
+        }
+        else
+        {
+            Debug.Log("You do not have enough to purchase this item");
+            return false;
+        }
+    }
+}
