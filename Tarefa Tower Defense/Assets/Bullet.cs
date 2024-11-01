166b5ebff01cb391d0749f5e7594c0a6b7b1a9a4
Pedrozhn <pedrohenriquenagy@gmail.com>
2024-11-01 14:22:30 -0300

    tirei os adendos e dps vou refazer eles certinho

--- Tarefa Tower Defense/Assets/Bullet.cs
+++ Tarefa Tower Defense/Assets/Bullet.cs
@@ -10,52 +10,52 @@ public class Bullet : MonoBehaviour
         instance = this;
     }
     [Header("References")]
-    [SerializeField] private Rigidbody2D rb; // Referência ao Rigidbody2D da bala
+    [SerializeField] private Rigidbody2D rb;
 
     [Header("Attributes")]
-    [SerializeField] private float bulletSpeed = 5f; // Velocidade da bala
-    [SerializeField] public float bulletdamage = 1f; // Dano causado pela bala
-    [SerializeField] private float lifetime = 5f; // Tempo de vida da bala antes de ser destruída
+    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] public float bulletdamage = 1f;
+    [SerializeField] private float lifetime = 5f; 
 
-    private Transform target; // Alvo que a bala deve seguir
+    private Transform target; 
 
     private void Start()
     {
-        // Destrói a bala após um tempo definido para evitar que fique na cena indefinidamente
+       
         Destroy(gameObject, lifetime);
     }
 
-    // Método para definir o alvo da bala
+   
     public void SetTarget(Transform _target)
     {
-        target = _target; // Atribui o alvo recebido à variável target
+        target = _target;
     }
 
     private void FixedUpdate()
     {
-        // Se não houver um alvo, não faz nada
+       
         if (!target) return;
 
-        // Calcula a direção em que a bala deve se mover em relação ao seu alvo
+       
         Vector2 direction = (target.position - transform.position).normalized;
 
-        // Aplica a velocidade à bala, movendo-a em direção ao alvo
+       
         rb.velocity = direction * bulletSpeed;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        // Tenta obter o componente Health do objeto com o qual a bala colidiu
+       
         Health enemyHealth = other.gameObject.GetComponent<Health>();
 
-        // Se o objeto tiver um componente Health, aplica dano
+
         if (enemyHealth != null)
         {
-            enemyHealth.TakeDamage(bulletdamage); // Aplica o dano à saúde do inimigo
-            // Aqui você pode adicionar um efeito visual ou sonoro para o impacto
+            enemyHealth.TakeDamage(bulletdamage); 
+           
         }
 
-        // Destrói a bala após colidir com qualquer objeto
+       
         Destroy(gameObject);
     }
 }
